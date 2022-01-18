27be284e453c9c2788de2275b2a9326bd17554fa
enPlace <n.place@protonmail.com>
2022-01-18 16:43:20 -0300

    Add basic css and get posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts").then((response) => {
+      setPosts(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      {posts.map((post) => {
+        console.log(post.title);
+        return (
+          <div class="post">
+            <div class="title">Title:{post.title}</div>
+            <div class="body">Content:{post.postText}</div>
+            <div class="footer">username:{post.username}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
