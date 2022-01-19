dcf5ce0d026b052bb0b3ea545473a720fcaa8830
enPlace <n.place@protonmail.com>
2022-01-19 15:24:35 -0300

    Add a form with validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
 function App() {
   return (
     <div className="App">
       <Router>
+        <Link to="/createpost">Create a New Post</Link>
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Home></Home>
+          </Route>
+          <Route exact path="/createpost">
+            <CreatePost></CreatePost>
           </Route>
         </Switch>
       </Router>
---
+++ client/src/pages/CreatePost.js
@@ -0,0 +1,68 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+const CreatePost = () => {
+  const initialValues = {
+    title: "",
+    postText: "",
+    username: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required(),
+    postText: Yup.string().required(),
+    username: Yup.string().min(3).max(15).required(),
+  });
+
+  const onSubmit = (data, { resetForm }) => {
+    console.log(data);
+    resetForm({
+      data: {
+        title: "",
+        postText: "",
+        username: "",
+      },
+    });
+    console.log(initialValues);
+    axios.post("http://localhost:3001/posts", data).then(() => {
+      console.log("success!", data);
+    });
+  };
+  return (
+    <div className="CreatePost">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form>
+          <label htmlFor="title">Title</label>
+          <ErrorMessage name="title" component="span" />
+          <Field
+            id="createTitle"
+            name="title"
+            placeholder="enter a title..."
+          ></Field>
+          <label htmlFor="postText">Post Text</label>
+          <ErrorMessage name="postText" component="span" />
+          <Field
+            id="createPostText"
+            name="postText"
+            placeholder="enter the text of your post..."
+          ></Field>
+          <label htmlFor="username">username</label>
+          <ErrorMessage name="username" component="span" />
+          <Field
+            id="createPostUserName"
+            name="username"
+            placeholder="enter your username..."
+          ></Field>
+          <button type="submit">Submit Post</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default CreatePost;
